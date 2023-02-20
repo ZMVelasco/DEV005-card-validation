e4e2e9ac3690307e8d2b01c5601eb5bf10b59918
Zaira  Velasco <zaira.vl21@gmail.com>
2023-02-19 23:12:50 -0600

    Doubled and multiplied

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import validator from "./validator.js";
-const cardNumberInput = document.getElementById("cardNumber");
+const cardNumberInput = document.getElementById("card-number");
 
 cardNumberInput.addEventListener("input", function (event) {
   const cardNumber = event.target.value.replace(/\D/g, "");
-  const formattedCardNumber = cardNumber.replace(/(\d{4})/g, "$1 ");
-  event.target.value = formattedCardNumber.trim();
+  const choppedCardNumber = cardNumber.replace(/(\d{4})/g, "$1 ");
+  event.target.value = choppedCardNumber.trim();
 });
 console.log(validator);
-//q:How do I push the code to github? I have a repository but I don't know how to push the code to it.
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,16 @@
-const validator = {
-  // ...
-};
+const validator = {};
+
+let initialCardNumber = document.getElementById("card-number");
+initialCardNumber.addEventListener("input", function (event) {
+  let reversedCardNumberArray = event.target.value.split("").reverse();
+  console.log(reversedCardNumberArray);
+
+  const doubledDigitsArray = reversedCardNumberArray.map((digit, index) => {
+    const doubledDigit = index % 2 === 1 ? digit * 2 : digit;
+    return doubledDigit > 9 ? doubledDigit - 9 : doubledDigit;
+  });
+
+  console.log(doubledDigitsArray);
+});
 
 export default validator;
