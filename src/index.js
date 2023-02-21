1fb84a917af3899010ef8569b58882d7e8c078e5
Zaira  Velasco <zaira.vl21@gmail.com>
2023-02-21 13:59:58 -0600

    Completed isValid function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import validator from "./validator.js";
 const cardNumberInput = document.getElementById("card-number");
+const validateBtn = document.getElementById("btnValidate");
 
 cardNumberInput.addEventListener("input", function (event) {
   const cardNumber = event.target.value.replace(/\D/g, "");
-  const choppedCardNumber = cardNumber.replace(/(\d{4})/g, "$1 ");
-  event.target.value = choppedCardNumber.trim();
+  const formattedCardNumber = cardNumber.replace(/(\d{4})/g, "$1 ");
+  event.target.value = formattedCardNumber.trim();
 });
 console.log(validator);
+
+validateBtn.addEventListener("click", function () {
+  const number = cardNumberInput.value;
+  validator.isValid(number);
+  console.log(validator.isValid(number));
+});
